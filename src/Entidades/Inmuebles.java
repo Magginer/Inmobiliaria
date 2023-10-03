ad91dcd70583e3042900206ea636212bc54e6ec5
Magginer <martin.novaresi@gmail.com>
2023-10-03 11:26:12 -0300

    asd
    asd

--- src/Entidades/Inmuebles.java
+++ src/Entidades/Inmuebles.java
@@ -10,5 +10,98 @@ package Entidades;
  * @author marti
  */
 public class Inmuebles {
+    private int idinmueble;
+    private String direccion;
+    private int altura;
+    private String tipo;
+    private int superficie;
+    private int precio;
+    private String zona;
+    private boolean estado;
+
+    public Inmuebles() {
+    }
+
+    public Inmuebles(int idinmueble, String direccion, int altura, String tipo, int superficie, int precio, String zona, boolean estado) {
+        this.idinmueble = idinmueble;
+        this.direccion = direccion;
+        this.altura = altura;
+        this.tipo = tipo;
+        this.superficie = superficie;
+        this.precio = precio;
+        this.zona = zona;
+        this.estado = estado;
+    }
+
+    public int getIdinmueble() {
+        return idinmueble;
+    }
+
+    public void setIdinmueble(int idinmueble) {
+        this.idinmueble = idinmueble;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public string getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(string tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getSuperficie() {
+        return superficie;
+    }
+
+    public void setSuperficie(int superficie) {
+        this.superficie = superficie;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public String getZona() {
+        return zona;
+    }
+
+    public void setZona(String zona) {
+        this.zona = zona;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Inmuebles{" + "idinmueble=" + idinmueble + ", direccion=" + direccion + ", altura=" + altura + ", tipo=" + tipo + ", superficie=" + superficie + ", precio=" + precio + ", zona=" + zona + ", estado=" + estado + '}';
+    }
+    
+    
     
 }
