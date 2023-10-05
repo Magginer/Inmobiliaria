0a90bd2021b02b4127bc3535faa0679f4a7678b4
Magginer <martin.novaresi@gmail.com>
2023-10-05 11:03:49 -0300

    sdf
    sdf

--- src/AccesData/ContratoData.java
+++ src/AccesData/ContratoData.java
@@ -14,5 +14,8 @@ public class ContratoData {
     
     private Connection con = null;
     
+    private final PropietarioData pd = new PropietarioData();
+    private final InmueblesData id = new InmueblesData();
+    private final InquilinoData ind = new InquilinoData();
     
 }
