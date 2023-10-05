b64b9355bfd3a590fd77ff1faf6a63a74d515f29
ExeG26 <141650654+ExeG26@users.noreply.github.com>
2023-10-05 11:55:50 -0300

    F

--- src/AccesData/ContratoData.java
+++ src/AccesData/ContratoData.java
@@ -17,28 +17,5 @@ public class ContratoData {
     
     private Connection con = null;
     
-    private final InmueblesData ind= new InmueblesData();
-    private final //InquilinoData inq = new InquilinoData();
     
-    
-    
-    public ContratoData(){
-        this.con = Conexion.getConexion();
-    }
-    
-    public void buscarContrato(){ //Como MRD nombro esto??? atte: Ignacio UwU
-        
-    }
-    
-    public void cancelacion(){  //el deletear contrato
-        
-    }
-    
-    public void Crear_Contrato(ContratoVenta){  //el guardar contrato
-        
-    }
-    
-    public void Renovacion(){  //el modificar contrato
-        
-    }
 }
