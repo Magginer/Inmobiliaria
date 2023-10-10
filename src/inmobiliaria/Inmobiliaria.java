569f46cf7d86bfae9adf630d377f36fa1565a8cf
Magginer <martin.novaresi@gmail.com>
2023-10-10 11:04:43 -0300

    as
    asd

--- src/inmobiliaria/Inmobiliaria.java
+++ src/inmobiliaria/Inmobiliaria.java
@@ -27,27 +27,28 @@ public class Inmobiliaria {
     public static void main(String[] args) {
         
 
-     //Propietario elsa = new Propietario(158742,"elsa", "balero","Chacras 568", 456788654,9,true);
-     //Propietario juan = new Propietario(87951396,"Juan", "Chamorro","calle falsa 123", 7895645,8,true);
+     Propietario elsa = new Propietario(158742,"elsa", "balero","Chacras 568", 456788654,9,true);
+     Propietario juan = new Propietario(87951396,"Juan", "Chamorro","calle falsa 123", 7895645,8,true);
      //PropietarioData prop=new PropietarioData();
      //prop.GuardarPropietario(juan);
      //prop.GuardarPropietario(elsa);
      
-     //Inquilino moni = new Inquilino (4, 48984,"Moni","Argento","Las Heras 546","Dardo Fuseneco", 4874984,true);
+     Inquilino moni = new Inquilino (4, 48984,"Moni","Argento","Las Heras 546","Dardo Fuseneco", 4874984,true);
      //InquilinoData inq = new InquilinoData();
      //inq.GuardarInquilino(pepe);
      //inq.ModificarInquilino(pepe);
      //inq.EliminarLogico(3);
      //inq.DeletearInquilino(48984);
      
-     //Inmuebles inm = new Inmuebles (5,"Honorio Pueyrredon", 456, "departamento", 80, 120000, "Caballito", true, juan);
+     Inmuebles inm = new Inmuebles (5,"Honorio Pueyrredon", 456, "departamento", 80, 120000, "Caballito", true, juan);
      //InmueblesData inms=new InmueblesData();
      //inms.GuardarInmueble(inm);
    
      
-     //Contrato con = new Contrato(LocalDate.of(2023, 4, 25), LocalDate.of(2024, 4, 25), 100000, true, moni, inm);
-     //ContratoData cont = new ContratoData();
+     Contrato con = new Contrato(LocalDate.of(2023, 4, 25), LocalDate.of(2024, 4, 25), 100000, true, moni, inm);
+     ContratoData cont = new ContratoData();
      //cont.guardarContrato(con);
+     cont.actualizarContrato(LocalDate.of(2024, 4, 25), LocalDate.of(2025, 4, 25), 151515, true, 8);
  
           
     
