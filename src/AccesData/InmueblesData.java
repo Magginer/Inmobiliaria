08d8c0ea46ce5622cc866cdd5b19d8497dc1580e
Javier M. Colombini <110408141+Khalraak@users.noreply.github.com>
2023-10-24 14:23:00 -0300

    asd
    asdadaasd

--- src/AccesData/InmueblesData.java
+++ src/AccesData/InmueblesData.java
@@ -174,7 +174,7 @@ public class InmueblesData {
     }
      public ArrayList<Inmuebles> InmueblesPorPropietarios(int idpropietario){
     
-         String sql = "SELECT idinmueble, direccion, altura, tipo, superficie, precio, zona FROM inmuebles WHERE idpropietario = ?";
+         String sql = "SELECT idinmueble, direccion, altura, tipo, superficie, precio, zona, estado FROM inmuebles WHERE idpropietario = ?";
 
     ArrayList<Inmuebles> inmuebles = new ArrayList<>();
     
@@ -192,7 +192,7 @@ public class InmueblesData {
             inmueble.setSuperficie(rs.getInt("superficie"));
             inmueble.setPrecio(rs.getInt("precio"));
             inmueble.setZona(rs.getString("zona"));
-            
+            inmueble.setEstado(rs.getBoolean("estado"));    // agregue estado aquiy en la sql
             inmuebles.add(inmueble);
         }
         
